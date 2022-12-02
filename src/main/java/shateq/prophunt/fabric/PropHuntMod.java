4bb04a1c10ff6ba3822201d17d1bc0f4b7f20344
Szymon <shateq21@gmail.com>
2022-12-02 19:33:46 +0100

    WORK!!!

--- src/main/java/shateq/prophunt/fabric/PropHuntMod.java
+++ src/main/java/shateq/prophunt/fabric/PropHuntMod.java
@@ -1,22 +1,48 @@
 package shateq.prophunt.fabric;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import me.xdrop.fuzzywuzzy.Extractor;
 import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.ToStringFunction;
+import me.xdrop.fuzzywuzzy.algorithms.WeightedRatio;
+import me.xdrop.fuzzywuzzy.model.BoundExtractedResult;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.List;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class PropHuntMod implements ClientModInitializer {
     public static final Logger LOG = LoggerFactory.getLogger(PropHuntMod.class);
+    public static int hardLimit = 60; //TODO CONFIG
+    public static final List<Item> operatorItems = List.of(Items.COMMAND_BLOCK, Items.AIR);
+    private static boolean operator = false;
+
+    public static <T> List<BoundExtractedResult<T>> sortedExtraction(String query, Collection<T> choices, ToStringFunction<T> toStr, ToStringFunction<T> opt) {
+        Extractor extractor = new Extractor();
+        extractor.extractTop(query, choices, toStr, new WeightedRatio());
+        return List.of();
+    }
+
+    public static boolean isOperator() {
+        if (operator) return true;
+        assert MinecraftClient.getInstance().player != null;
+        return MinecraftClient.getInstance().player.hasPermissionLevel(2);
+    }
 
     @Override
     public void onInitializeClient() {
+        operator = false; //1.19.3 operator items
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
             LOG.debug("Registering helper commands");
 
--- src/main/java/shateq/prophunt/mixin/MixinCreativeInventoryScreen.java
+++ src/main/java/shateq/prophunt/mixin/MixinCreativeInventoryScreen.java
@@ -1,17 +1,24 @@
 package shateq.prophunt.mixin;
 
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.model.BoundExtractedResult;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenTexts;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import shateq.prophunt.fabric.PropHuntMod;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class MixinCreativeInventoryScreen extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
@@ -21,54 +28,38 @@ public abstract class MixinCreativeInventoryScreen extends AbstractInventoryScre
     public MixinCreativeInventoryScreen(PlayerEntity player) {
         super(new CreativeInventoryScreen.CreativeScreenHandler(player), player.getInventory(), ScreenTexts.EMPTY);
     }
-        /*List<BoundExtractedResult<String>> res = FuzzySearch.extractAll("goolge",
-            Arrays.asList("google", "bing", "facebook", "linkedin", "twitter", "googleplus", "bingnews", "plexoogl"),
-            i -> i.toLowerCase()
-        );
-        List<BoundExtractedResult<String>> sorted = FuzzySearch.extractSorted("goolge",
-            Arrays.asList("google", "bing", "facebook", "linkedin", "twitter", "googleplus", "bingnews", "plexoogl"),
-            i -> i.toLowerCase()
-        );*/
 
     @Inject(method = "search", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen$CreativeScreenHandler;scrollItems(F)V"))
     protected void search(CallbackInfo ci) {
-        final String string = this.searchBox.getText();
+        final String search = this.searchBox.getText().toLowerCase(Locale.ROOT);
         var handler = (CreativeInventoryScreen.CreativeScreenHandler) this.handler;
-        if (string.isEmpty() || string.startsWith("#")) {
-            return;
+        if (search.isEmpty() || search.startsWith("#")) return;
+        handler.itemList.clear(); //TODO subject
+
+        var items = Registry.ITEM.stream().toList();
+        var allSorted =
+            FuzzySearch.extractSorted(search, items,
+                item -> item.toString().replaceAll("_", " ")
+            );
+
+        ArrayList<ItemStack> stacks = new ArrayList<>();
+        for (BoundExtractedResult<Item> i : allSorted) {
+            if (!PropHuntMod.isOperator() && PropHuntMod.operatorItems.contains(i.getReferent())) continue;
+            if (i.getScore() < PropHuntMod.hardLimit) continue;
+
+            var item = i.getReferent();
+            stacks.add(item.getDefaultStack());
         }
 
-        try {
-            for (int i = 0; i < handler.itemList.size(); i++) {
-                if (i % 2 == 0) {
-                    handler.itemList.set(i, Items.CARROT.getDefaultStack());
-                }
+        this.handler.itemList.addAll(stacks);
+        /*for (Item item : Registry.ITEM) {
+            final String q1 = item.toString();
+            final String q2 = item.getName().getString();
+            //final int ratio = FuzzySearch.ratio(search.toLowerCase(Locale.ROOT), query);
+            final int partial = FuzzySearch.partialRatio(search.toLowerCase(Locale.ROOT), q1);
+            if (ratio > 50 || partial > 50) {
+                handler.itemList.add(item.getDefaultStack());
             }
-        } catch (IndexOutOfBoundsException e) {
-            PropHuntMod.LOG.error("Tried:", e);
-        }
-
-        //TODO here
-        /*ThreadedItemLooping thread = new ThreadedItemLooping(string, this.handler.itemList);
-        thread.start();
-        for (final Item i : Registry.ITEM) {
-            final String query = i.toString().toLowerCase(Locale.ROOT);
-
-            final int ratio = FuzzySearch.ratio(string.toLowerCase(Locale.ROOT), query);
-            PropHuntMod.LOG.info("{} and {}", query, ratio);
-
-            if (ratio > 50) {
-                this.handler.itemList.add(
-                    new ItemStack(i)
-                );
-            }
-        }
-        List<BoundExtractedResult<ItemStack>> results;
-        Registry.ITEM.streamEntries().
-            Stream < Item > itemStream = Registry.ITEM.stream();
-        results = FuzzySearch.extractAll(string.toLowerCase(Locale.ROOT), itemStream, i -> i.toString());
-        this.handler.itemList.addAll(itemList.forEach(Item::getDefaultStack));
-        */
+        }*/
     }
 }
-
